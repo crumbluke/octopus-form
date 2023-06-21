c948698b2048dd5ac95e0bc2e36328e40666a465
crumbluke <98920295+crumbluke@users.noreply.github.com>
2023-06-21 15:26:15 -0400

    Add files via upload

---
+++ Actions.js
@@ -0,0 +1,5 @@
+const done = document.getElementById("submission");
+
+done.addEventListener('submit', function() {
+  open("file:///C:/Users/crumb/OneDrive/Documents/Octopus%20form/Test.html");
+});
